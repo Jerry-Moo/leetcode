16b9b69e493846a586b5d1de53bbe9c63b029285
JerryMoo <jerrymoo@JerryMoodeMacBook-Air.local>
2019-05-31 19:54:45 +0800

    100 same tree

---
+++ 100.same_tree/same_tree.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if  p is None and q is None :
+            return True
+        elif p is not None and q is not None and p.val == q.val:
+            return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+        else:
+            return False
+
