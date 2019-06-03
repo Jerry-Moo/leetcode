50a73264717d4583a33c18d101a8f9c5098f9615
wuchujie <wuchujie@Mac-mini-2.local>
2019-06-03 17:18:57 +0800

    070 climbing stairs

--- 070.climbing_stairs/climbing_stairs.py
+++ 070.climbing_stairs/climbing_stairs.py
@@ -1,7 +1,21 @@
+import time
 class Solution(object):
     def climbStairs(self, n):
         """
         :type n: int
         :rtype: int
         """
-        
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        else:
+            res = 0
+            i, j = 1, 2
+            k = 3
+            while (k <= n):
+                res = i + j 
+                i = j
+                j = res
+                k += 1
+        return res
